c660fcbded751fd29225e6b17f195cdfd3ea5928
pro100mq <144606932+pro100mq@users.noreply.github.com>
2025-07-20 18:02:06 +0300

    update

--- src/js/renderStudents.js
+++ src/js/renderStudents.js
@@ -1,23 +1,3 @@
-// export function renderStudents(students) {
-//   return students.map((obj) => {
-//     return `
-//       <tr id="${obj.id}">
-//         <td name="id">${obj.id}</td>
-//         <td>${obj.name}</td>
-//         <td>${obj.age}</td>
-//         <td>${obj.course}</td>
-//         <td>${obj.skills}</td>
-//         <td>${obj.email}</td>
-//         <td>${obj.isEnrolled}</td>
-//         <td><button type="button" class="delete-btn">Видалити</button></td>
-//         <td><button type="button" class="update-btn">Редагувати</button></td>
-//       </tr>
-//     `;
-//   })
-//   .join('');
-// }
-
-
 export function renderStudents(students) {
     const objectChange = students
     .map((object) => {
